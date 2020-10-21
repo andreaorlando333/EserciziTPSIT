03ac8b5df3aafb12908e3b928b603d2fadf38d35
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2020-10-21 10:56:36 +0200

    Add files via upload

---
+++ C/palindromo-orlando.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char inpout[] = 'kayak';
+    int isPalindrome = 0;
+    int lenght = 0;
+
+    for(int i=0; input[i] != '/0'; i++)
+        lenght = i-l;
+
+    for(int i = 0; i<lenght/2;i++){
+        int j= lenght;
+        ispalindrome =(input[i] == input[j])*1;
+        j--;
+
+
+    }    
+    return 0;
+}
