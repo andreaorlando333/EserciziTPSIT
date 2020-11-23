19fc81689dae2abf786f7f4d55121584d1f2da92
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2020-11-23 12:30:36 +0100

    Add files via upload

---
+++ Python/TurtleGame.py
@@ -0,0 +1,17 @@
+import turtle
+
+robot = turtle.Turtle 
+win = turtle.Screen
+
+win.title("Gioco 1")
+win.bgcolor("green")
+win.setup(width=800, height=800)
+
+def jump:
+	robot.forward(50)
+	print("Jump")
+
+win.listen()
+win.onkey(jump, "space")
+
+win.mainloop()
