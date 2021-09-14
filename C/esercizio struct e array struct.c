f6beb786f63de61816088b8179e889c021f46f91
Andrea Orlando <71812411+andreaorlando333@users.noreply.github.com>
2021-09-14 11:41:44 +0200

    Add files via upload

---
+++ C/esercizio struct e array struct.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX = 100;
+
+typedef struct {
+	char cognome[50];
+	char nome[50];
+	float voti[15];
+} Alunno;
+
+
+int main(){
+
+	Alunno studente;
+	studente.voti[0] = 6;
+	strcpy(studente.cognome, "rossi");
+	strcpy(studente.nome, "marco");
+	alunno classe [MAX];
+
+	for(int k=0, k<MAX, k++){
+
+		classe[k].voti[0] = 6;
+		strcpy(classe[i].cognome, "rossi");
+		strcpy(classe[i].nome, "marco");
+
+	}
+
+}
+
